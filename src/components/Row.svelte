b2a04b14bb8e11b77a76d35fe02cf3f76d71dc15
Josh Nussbaum <joshnuss@gmail.com>
2019-11-22 22:43:06 -0500

    Adds hover effect

--- src/components/Row.svelte
+++ src/components/Row.svelte
@@ -4,4 +4,10 @@
   const dispatch = createEventDispatcher();
 </script>
 
+<style>
+  tr:hover {
+    background: #eee
+  }
+</style>
+
 <tr on:click|preventDefault={() => dispatch('click')}><slot/></tr>
